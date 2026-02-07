b4d107c5598bfa9be807a897022ed2dc290eddac
Petrus <petrus.heikkila@outlook.com>
2026-02-07 21:34:38 +0200

    secpmd commit for week 3

--- src/main/java/com/o3/server/ObservationRecord.java
+++ src/main/java/com/o3/server/ObservationRecord.java
@@ -1,6 +1,8 @@
 package com.o3.server;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class ObservationRecord {
     private String targetBodyName;
@@ -16,13 +18,36 @@ public class ObservationRecord {
         this.epoch = json.getString("epoch");
 
         if (json.has("orbital_elements")) {
-            this.orbitalElements = json.getJSONObject("orbital_elements");
+            JSONObject orb_elements = json.getJSONObject("orbital_elements");
+
+            orb_elements.getDouble("semi_major_axis_au");
+            orb_elements.getDouble("eccentricity");
+            orb_elements.getDouble("inclination_deg");
+            orb_elements.getDouble("longitude_ascending_node_deg");
+            orb_elements.getDouble("argument_of_periapsis_deg");
+            orb_elements.getDouble("mean_anomaly_deg");
+
+            this.orbitalElements = orb_elements;
         }
 
         if (json.has("state_vector")) {
-                this.stateVector = json.getJSONObject("state_vector");
+            JSONObject s_vector = json.getJSONObject("state_vector");
+
+            validateNumericArray(s_vector.getJSONArray("position_au"), 3);
+            validateNumericArray(s_vector.getJSONArray("velocity_au_per_day"), 3);
+
+            this.stateVector = s_vector;
         }
-        
+
+    }
+
+    private void validateNumericArray(JSONArray array, int expectedSize) throws JSONException {
+            if (array.length() != expectedSize) {
+                throw new JSONException("Wrong number of elements in array");
+            }
+            for (int i = 0; i < array.length(); i++) {
+                array.getDouble(i);
+            }
     }
 
     public JSONObject toJSON() {
